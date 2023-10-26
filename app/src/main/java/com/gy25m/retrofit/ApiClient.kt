3e8d824d31a8db2a781c88d3c09bdb0703a1ceb3
kim gyeom <kimgyeom2@gmail.com>
2023-10-26 14:14:07 +0900

    ㅋ

--- app/src/main/java/com/gy25m/retrofit/ApiClient.kt
+++ app/src/main/java/com/gy25m/retrofit/ApiClient.kt
@@ -1,19 +1,29 @@
 package com.gy25m.retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
 
-        private const val BASE_URL = "https://jsonplaceholder.typicode.com/comments/"
+    private const val BASE_URL = "https://jsonplaceholder.typicode.com/comments/"
 
-    var retrofit=Retrofit.Builder()
-    .baseUrl(BASE_URL)
-    .addConverterFactory(GsonConverterFactory.create())
-    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-    .build()
-    .create(ApiService::class.java)
+    //  .addInterceptor를추가해 로그 같은거 추가하면 더 좋음
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(1, TimeUnit.MINUTES)
+        .writeTimeout(1, TimeUnit.MINUTES)
+        .build()
+
+    var retrofit = Retrofit.Builder()
+        .client(okHttpClient)
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
+        .create(ApiService::class.java)
 
 }
